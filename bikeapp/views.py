d8c73c0073083229c1d00d80cd5f29ffbc10ad2b
phin09 <sy6kang@gmail.com>
2020-10-14 17:54:34 +0900

    update views.py

--- bikeapp/views.py
+++ bikeapp/views.py
@@ -5,6 +5,7 @@ import json
 import requests
 from time import ctime
 import time
+import sqlite3
 # Create your views here.
 
 def index(request):
@@ -24,20 +25,41 @@ def bikeMap(request):
             api_dp = pd.DataFrame(api_dict)
             seoulbike = pd.concat([seoulbike, api_dp])
 
-        st_dict = seoulbike.to_dict(orient='records')
-        '''
-        모델에 집어넣기
-        '''
+        now = time.localtime()
+        now_time = time.strftime("%Y/%m/%d %H:%M:%S", now)
+        seoulbike = seoulbike.drop_duplicates("stationId", keep="last")
+        seoulbike.insert(7, "date", now_time)
+        seoulbike = seoulbike.reset_index()
+        seoulbike = seoulbike.drop('index', axis=1)
 
-        # now = time.localtime()
-        # now_time = time.strftime("%Y/%m/%d %H:%M:%S", now)
-        # seoulbike = seoulbike.drop_duplicates("stationId", keep="last")
-        # seoulbike.insert(7, "date", now_time)
-        # seoulbike = seoulbike.reset_index()
-        # seoulbike = seoulbike.drop('index', axis=1)
-        # seoulbike.to_csv("test.csv", encoding="cp949")
+        # sqlite3 db 'station' table에 데이터 반영
+        con = sqlite3.connect('./db.sqlite3')
+        cur = con.cursor()
+        # cur.execute("DELETE FROM station")
+        # station table에 데이터 넣기
+        seoulbike.to_sql('station', con, if_exists='replace')
+        # if_exists = 'fail' : 같은 이름의 Table이 존재할 경우 ValueError 가 남
+        # if_exists = 'replace': 같은 이름의 Table이 존재할 경우 기존 Table을 Drop하고 새로운 값을 Insert함
+        # if_exists = 'append': 같은 이름의 Table이 존재할 경우 기존 Table에 추가로 새로운 값을 Insert함
+        con.commit()
+        con.close()
+
+        # sqlite3 db station table에 데이터 검색
+
+
+        seoulbike.to_csv("test.csv", encoding="cp949")
+
 
     except Exception as e:
         api = "Error..."
+    # sqlite3 db 'station' table data load
+    con = sqlite3.connect('./db.sqlite3')
+    cur = con.cursor()
+    query = cur.execute(("select * from station"))
+    cols = [column[0] for column in query.description]
+    bike_load = pd.DataFrame.from_records(data=query.fetchall(), columns=cols)
+    con.close()
+    st_dict = bike_load.to_dict(orient='records')
 
-    return render(request, 'map.html', {'api': api_result, 'api_dict': st_dict})
+    return render(request, 'map.html', {'api_dict': st_dict})
+
