98ebcbca1651564f638d6dceb2cf7fc789c2349e
parkjunha <junha6316@gmail.com>
2020-11-30 01:08:10 +0900

    modify seed_users command

--- Django/user/management/commands/seed_users.py
+++ Django/user/management/commands/seed_users.py
@@ -4,27 +4,27 @@ from sys import stdout
 from django.core.management.base import BaseCommand
 from django.contrib.auth.hashers import make_password
 
-import account.models as account_model
+import user.models as user_model
 
 NAME = "USER"
-
 
 class Command(BaseCommand):
     help = "read user.csv and write to Database"
 
     def handle(self, *args, **kwargs):
-        with open('DEV/users.csv', newline='') as csvfile:
+        with open('./users.csv', newline='') as csvfile:
             rows = csv.reader(csvfile, delimiter=',', quotechar='|')
 
             for idx, row in enumerate(rows):
                 if idx == 0:
                     continue
-                username = row[0]
+
+                login_Id = row[0]
                 password = make_password(row[1])
                 areaId = int(row[2])
 
-                obj = account_model.Users.objects.create(
-                    username=username,
+                obj = user_model.Users.objects.create(
+                    login_Id=login_Id,
                     password=password,
                     areaId=areaId
                 )
