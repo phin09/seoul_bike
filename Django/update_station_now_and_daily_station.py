e77d3e5827ed6f93b21e1ce6846167a005726750
parkjunha <junha6316@gmail.com>
2020-12-03 00:06:23 +0900

    little change for update_stationNow

--- Django/update_station_now_and_daily_station.py
+++ Django/update_station_now_and_daily_station.py
@@ -12,13 +12,15 @@ import django
 from django.shortcuts import get_object_or_404
 from django.utils import dateformat, timezone
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "seoulbike.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 django.setup()
-from bikeapp.models import StationNow, Area, DailyStation
+
+from record.models import StationNow
+from station.models import Stations
 from custom import get_secret
 
 # 따릉이 api 호출
-SEOUL_KEY = get_secret('secrets.json', "SEOUL_KEY")
+SEOUL_KEY = os.environ.get("SEOUL_KEY")
 
 api_urls = ["http://openapi.seoul.go.kr:8088/"+SEOUL_KEY+"/json/bikeList/1/1000",
                 "http://openapi.seoul.go.kr:8088/"+SEOUL_KEY+"/json/bikeList/1001/2000",
@@ -35,48 +37,53 @@ try:
             stationName = str(item['stationName'])
             dataId = int(stationName.split('.')[0])
             try:  # 대여소가 api에도 table area에도 있는 경우 -> update
-                check_obj = Area.objects.get(pk=dataId)
+                check_obj = Stations.objects.get(pk=dataId)
+                print("Yes")
                 stationCode = str(item['stationId'])
                 parkingBikeTotCnt = int(item['parkingBikeTotCnt'])
 
                 try:  # update table station_now
-                    station_obj = StationNow.objects.get(pk=stationCode)
-                    station_obj.parkingBikeTotCnt = parkingBikeTotCnt
-                    station_obj .created_at = timezone.localtime()
-                    station_obj.save()
+                    station = Stations.objects.get(dataId=dataId)
+                    stationNow = StationNow.objects.create(
+                        parkingBikeTotCnt = parkingBikeTotCnt, 
+                        station = station
+                        )
+                    
+                    stationNow.save()
+
                 except Exception as e:
                     print(e)
 
-            except Area.DoesNotExist:   # 대여소가 api에는 있지만 table area에 없는 경우 -> pass
+            except Stations.DoesNotExist:   # 대여소가 api에는 있지만 table area에 없는 경우 -> pass
                 pass
 
 except Exception as e:
     print(e)
 
-try:    # update table daily_station
-    queryset = StationNow.objects.all()
-    for item in queryset:
-        stationCode = item.stationCode
-        # matching query does not exist 방지
-        check = Area.objects.filter(stationCode=stationCode)
-        if len(check) > 0:
-            dataId = get_object_or_404(Area, stationCode=stationCode)
-            parkingBikeTotCnt = int(item.parkingBikeTotCnt)
-            formatted_date = dateformat.format(timezone.localtime(item.created_at), 'Y-m-d H:i')
-            try:
-                c = DailyStation.objects.create(
-                    dataId=dataId,
-                    parkingBikeTotCnt=parkingBikeTotCnt,
-                    created_at=formatted_date,
-                )
-                c.save()
-            except Exception as e:
-                print(e)
-        else:
-            pass
+# try:    # update table daily_station
+#     queryset = StationNow.objects.all()
+#     for item in queryset:
+#         stationCode = item.stationCode
+#         # matching query does not exist 방지
+#         check = Stations.objects.filter(stationCode=stationCode)
+#         if len(check) > 0:
+#             dataId = get_object_or_404(, stationCode=stationCode)
+#             parkingBikeTotCnt = int(item.parkingBikeTotCnt)
+#             formatted_date = dateformat.format(timezone.localtime(item.created_at), 'Y-m-d H:i')
+#             try:
+#                 c = DailyStation.objects.create(
+#                     dataId=dataId,
+#                     parkingBikeTotCnt=parkingBikeTotCnt,
+#                     created_at=formatted_date,
+#                 )
+#                 c.save()
+#             except Exception as e:
+#                 print(e)
+#         else:
+#             pass
 
-except Exception as e:
-    print(e)
+# except Exception as e:
+#     print(e)
 
 
 # queryset = DailyStation.objects.all()
